47229fb32b93acabf3caf797a04d0ce6a821d212
sandinook <129908232+sandinok@users.noreply.github.com>
2025-07-18 20:04:17 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/sandinowebtest/', // <--- LÍNEA AÑADIDA
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  // Esta es la línea más importante.
+  // Le dice a tu proyecto que vive dentro de la carpeta /sandinowebtest/
+  base: '/sandinowebtest/',
+  
+  plugins: [react()],
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
