8abe70ed4f66f18a9cc8aff645d613b89f05048c
sandinook <129908232+sandinok@users.noreply.github.com>
2025-07-18 20:19:22 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // <--- CAMBIO IMPORTANTE
 import App from './App.tsx';
 import './index.css';
 
-// 1. Se crea el router
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      // Si en el futuro tienes más páginas (ej: /sobre-mi), las añades aquí
-    },
-  ],
-  {
-    // 2. Se le dice al router que la página base es /sandinowebtest/
-    basename: "/sandinowebtest", 
-  }
-);
-
-// 3. Se renderiza la aplicación usando el router
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
+// Se renderiza la aplicación usando HashRouter
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
